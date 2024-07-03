200d50e6eb523c8a115ab0ea0e8e9997b0d6ffdf
El Rakkai Omar Wahid <ggg153563@gmail.com>
2024-07-03 09:40:42 +0700

    push git

---
+++ resources/js/Pages/Test.jsx
@@ -0,0 +1,13 @@
+import React, { useState} from 'react';
+
+const Test = () => {
+    return(
+        <>
+            <h1>letseeee
+                
+            </h1>
+        </>
+    )
+}
+
+export default Test
